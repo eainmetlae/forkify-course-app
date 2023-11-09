09eb02ddebd8f087587cb1debeb5d0151d08f959
eainmetlae <eainmetlae@gmail.com>
2023-11-09 21:36:17 +0630

    bug fix for resetting form after uploading recipe

--- src/js/view/addRecipeView.js
+++ src/js/view/addRecipeView.js
@@ -30,6 +30,7 @@ class AddRecipeView extends View {
   addHandlerUpload(handler) {
     this._parentElement.addEventListener('submit', function (e) {
       e.preventDefault();
+
       const dataArr = [...new FormData(this)]; //this here is form
       const data = Object.fromEntries(dataArr);
       handler(data);
--- src/js/view/recipeView.js
+++ src/js/view/recipeView.js
@@ -63,7 +63,7 @@ class RecipeView extends View {
               <use href="${icons}#icon-clock"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--minutes">${
-              this._data.cookTime
+              this._data.cookingTime
             }</span>
             <span class="recipe__info-text">minutes</span>
           </div>
